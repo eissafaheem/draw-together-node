fb57014c60b9dff51b67fe869f2d241af56e7830
Eissa Faheem <eissafaheem@gmail.com>
2023-11-01 14:26:02 +0530

    Feat: live drawing with socket io

--- server.js
+++ server.js
@@ -6,4 +6,30 @@ const io = new Server(8000, {
 
 io.on("connection",(socket)=>{
     console.log(socket.id)
+    socket.on("joinRoom",(roomId)=>{
+        socket.roomId = roomId;
+        socket.join(roomId);
+    })
+    socket.on("startDrawing", (data)=>{
+        console.log("start")
+        socket.in(socket.roomId).emit("startDrawing", data)
+    })
+    socket.on("drawingData", (data)=>{
+        console.log("dd", data)
+        socket.in(socket.roomId).emit("drawingData", data);
+    })
+    socket.on("finishDrawing", ()=>{
+        console.log("finish")
+        socket.in(socket.roomId).emit("finishDrawing");
+    })
+    socket.on("clearCanvas", ()=>{
+        console.log("clr")
+        socket.in(socket.roomId).emit("clearCanvas");
+    })
+    socket.on("cursorMove", (data)=>{
+        console.log("move", data)
+        const {offsetX, offsetY} = data;
+        const socketId = socket.id;
+        socket.in(socket.roomId).emit("cursorMove", {socketId, offsetX, offsetY});
+    })
 })
