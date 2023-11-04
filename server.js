b7514a60ebbfc97d6f6dd89ac63bd8bd10f88a83
Eissa Faheem <eissafaheem@gmail.com>
2023-11-04 10:52:40 +0530

    Feat: unique cursor for each user

--- server.js
+++ server.js
@@ -4,31 +4,45 @@ const io = new Server(8000, {
     cors: true
 })
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     console.log(socket.id)
-    socket.on("joinRoom",(roomId)=>{
+    const socketId = socket.id;
+    socket.on("joinRoom", (roomId) => {
         socket.roomId = roomId;
         socket.join(roomId);
     })
-    socket.on("startDrawing", (data)=>{
+    socket.on("startDrawing", (data) => {
         console.log("start")
         socket.in(socket.roomId).emit("startDrawing", data)
     })
-    socket.on("drawingData", (data)=>{
-        console.log("dd", data)
-        socket.in(socket.roomId).emit("drawingData", data);
+    socket.on("drawingData", (data) => {
+        // console.log("dd", data)
+        const {
+            clientX,
+            clientY,
+            offsetX,
+            offsetY,
+            color
+        } = data;
+        socket.in(socket.roomId).emit("drawingData", {
+            socketId, 
+            clientX,
+            clientY,
+            offsetX,
+            offsetY,
+            color
+        });
     })
-    socket.on("finishDrawing", ()=>{
+    socket.on("finishDrawing", () => {
         console.log("finish")
         socket.in(socket.roomId).emit("finishDrawing");
     })
-    socket.on("clearCanvas", ()=>{
+    socket.on("clearCanvas", () => {
         console.log("clr")
         socket.in(socket.roomId).emit("clearCanvas");
     })
-    socket.on("cursorMove", (data)=>{
-        const {clientX, clientY} = data;
-        const socketId = socket.id;
-        socket.in(socket.roomId).emit("cursorMove", {socketId,  clientX,  clientY});
+    socket.on("cursorMove", (data) => {
+        const { clientX, clientY } = data;
+        socket.in(socket.roomId).emit("cursorMove", { socketId, clientX, clientY });
     })
 })
