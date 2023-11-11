9d82a01d3c88e59dcf8f73a2c5ca2799427a39e5
Eissa Faheem <eissafaheem@gmail.com>
2023-11-11 11:59:08 +0530

    Feat: added .env

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const { Server } = require("socket.io");
+const dotenv = require('dotenv').config();
+const port  = process.env.PORT || 500
 
-const io = new Server(8000, {
+const io = new Server(port, {   
     cors: true
 })
 
